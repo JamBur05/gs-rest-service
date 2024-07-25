9216fe8c99999b1d1fb910fc1e588375ab56dbd1
James Burge <james3906@gmail.com>
2024-07-25 16:54:14 +0100

    JUnit tests written for API

--- initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
+++ initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -1,14 +1,56 @@
 package com.example.restservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class RestServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	// Test POST request for creating employees
 	@Test
-	public void contextLoads() {
+	public void shouldCreateEmployee() throws Exception {
+		Employee employee = new Employee(1, "John", "Doe", "john.doe@example.com", "Developer");
+
+		mockMvc.perform(post("/employees")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(employee)))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Employee added!"));
+	}
+
+	// Test GET request for fetching employees
+	@Test
+	public void shouldGetEmployees() throws Exception {
+		mockMvc.perform(get("/employees")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
 	}
 
 }
